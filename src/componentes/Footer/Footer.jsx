b15eb12335306af3feeb81f71a1929cb203afbd9
Daniel Alvarado <danieltrm1810@gmail.com>
2022-01-06 17:58:20 -0300

    responsive del footer, se cambian nombre de clases

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -4,10 +4,10 @@ function Footer() {
     return (
         <div className="footer">
             <div className="container">
-            <h1 className="title">Conoce más acerca <br></br> de nosotros</h1>
-            <button className="btn-click">¡Haz click aquí!</button>
+            <h1 className="title-footer">Conoce más acerca <br></br> de nosotros</h1>
+            <button className="btn-click-footer">¡Haz click aquí!</button>
             </div>
-            <h2 className="derechos">Derechos reservados 2021</h2>
+            <h2 className="derechos-footer">Derechos reservados 2021</h2>
         </div>
     )
 }
