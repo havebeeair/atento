9b554a49d70e6677d0bfdd6cce75f6f060276889
Daniel Alvarado <danieltrm1810@gmail.com>
2022-01-06 16:59:25 -0300

    Se realiza componente Footer con respectivo Footercss

---
+++ src/componentes/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <div className="container">
+            <h1 className="title">Conoce más acerca <br></br> de nosotros</h1>
+            <button className="btn-click">¡Haz click aquí!</button>
+            </div>
+            <h2 className="derechos">Derechos reservados 2021</h2>
+        </div>
+    )
+}
+
+export default Footer;
