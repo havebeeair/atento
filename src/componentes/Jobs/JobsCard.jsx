c1f02af58edf07d621ae3d41cdece7fb3b6b6465
Luis Escalona <luiscescalona@gmail.com>
2022-01-09 21:24:46 -0300

    Chances on jobs component

--- src/componentes/Jobs/JobsCard.jsx
+++ src/componentes/Jobs/JobsCard.jsx
@@ -4,11 +4,19 @@ function JobsCard(props) {
   return (
     <>
       <div className="jobs-card">
-        <img src={props.img} alt="" />
+        <img src={props.img || props.logo} alt="" />
         <div>
           <h3>{props.title}</h3>
-          <h5>Ciudad de México</h5>
-          <a href="">Ver más</a>
+          <h5>
+            {props.location.length > 0 ? props.location[0].display_name : null}
+          </h5>
+          <a
+            href={`https://apply.worcket.com/?id=${props.id}`}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Ver más
+          </a>
         </div>
       </div>
     </>
